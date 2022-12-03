2472fc9d2ca26d683c7520898f16d2ca468da533
JuMojo <julien@mo-jo.fr>
2022-12-03 14:03:38 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedRessources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation.",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learning to google...",
+          link: "https://google.org",
+        },
+      ],
+    };
+  },
+};
+</script>
